2e4f1828da3762d707a6d295db2000a123fc5a72
HughBryan <hughbryan@gmail.com>
2025-01-24 12:16:34 +1100

    Added helper files made years ago

---
+++ Helper Files/rental_json.py
@@ -0,0 +1,54 @@
+import os
+import json
+
+
+
+path = "Images/Oscar_Resources/RENTAL PHOTOS"
+
+data_ls = []
+json_data = json
+data_header = {}
+
+for folder in os.listdir(path):
+    folder_path = path+"/"+folder
+
+    section = folder_path.split("/")
+    section = section[len(section)-1]
+    print(section)
+
+    section_data = []
+
+    for file in os.listdir(folder_path):
+        data = {}
+        file_path = folder_path+"/"+file
+
+        if file.startswith("._"):
+            continue
+
+        if file_path.endswith(".png") or file_path.endswith("jpg") or file_path.endswith("jpeg") or file_path.endswith(".webp"):
+
+            try:
+
+                data["image"] = file_path
+                price = file_path.strip(".webp").strip(".jpeg").strip(".jpg").split("$")
+                data["price"] = price[len(price)-1].strip()
+                name = file_path.split("\uf022\uf022")[0].split("/")
+                name = name[len(name)-1].replace(".","").replace("_","").strip()
+                data['name'] =name
+                
+
+                section_data.append(data)
+
+
+            except: print("failed: "+file_path)
+
+
+
+    data_header[section] = section_data
+
+json_data = json.dumps(data_header)
+
+
+# Writing to sample.json
+with open("rental.json", "w") as outfile:
+    outfile.write(json_data)
